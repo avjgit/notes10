c49cbcb769f5de3b4d66dbe44f8fe92010ff6ab5
avjgit <avjmail@gmail.com>
2015-09-06 10:44:36 +0300

    udacity intro date diff cleanup

--- udacity_intro/age_in_days.py
+++ udacity_intro/age_in_days.py
@@ -20,9 +20,19 @@ def isLeapYearFebruary(y, m):
 def daysOfMonth(y,m):
     return 29 if isLeapYearFebruary(y,m) else daysOfMonths[m-1]
 
+def daysBetweenMonths(y, m1, d1, m2, d2):
+    days = 0
+    for month in range(m1+1,m2):
+        days += daysOfMonth(y,month)
+    endOfStartMonth = daysOfMonth(y,m1)
+    days += endOfStartMonth - d1
+    days += d2
+    return days
+
 def daysBetweenDates(y1, m1, d1, y2, m2, d2):
 
     days = 0
+
     for year in range(y1,y2):
         if (isLeapYear(year) and m1 <= 2) or isLeapYear(year+1):
             days += 366
@@ -30,38 +40,11 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
             days += 365
 
     if(m2 > m1):
-        # # # # print 'm2>'
-        for month in range(m1+1,m2):
-            # # # # print month
-            days += daysOfMonth(y2,month)
-            # # # # print days
-        endOfStartMonth = daysOfMonth(y2,m1)
-        days += endOfStartMonth - d1
-        days += d2
+        days += daysBetweenMonths(y2,m1,d1,m2,d2)
     elif(m2 < m1):
-        # print 'm2<'
-        for month in range(m2+1,m1):
-            # print 'month ' + str(month)
-            # print days
-            # print daysOfMonth(y2,month)
-            days -= daysOfMonth(y2,month)
-        # print days
-
-        endOfEndMonth = daysOfMonth(y2,m2)
-        # print '?'
-        # print endOfEndMonth - d2
-        days -= endOfEndMonth - d2
-        # print days
-
-        days -= d1
-        # print days
-
+        days -= daysBetweenMonths(y2,m2,d2,m1,d1)
     else:
-        # # # # print 'm2='
-        if (d2 > d1):
-            days += d2-d1
-        else: 
-            days -= d1-d2
+        days += d2-d1 #handles also negative difference
 
     return days
 
@@ -75,11 +58,6 @@ days = daysBetweenDates(2000, 5, 1, 2001, 3, 1); print days, days == 304
 days = daysBetweenDates(2000, 5, 15, 2001, 5, 15); print days, days == 365
 days = daysBetweenDates(2000, 5, 15, 2001, 3, 2); print days, days == 291
 days = daysBetweenDates(2000, 5, 15, 2010, 3, 2); print days, days == 3578
-
-
-
-
-
-
-
+days = daysBetweenDates(2000, 1, 10, 2001, 1, 20); print days, days == 376
+days = daysBetweenDates(2000, 1, 20, 2001, 1, 10); print days, days == 356
 
