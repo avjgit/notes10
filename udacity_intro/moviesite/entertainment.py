ce6c9632943b1660b43bbf0e2f5cea690a950600
avjgit <avjmail@gmail.com>
2015-09-14 12:53:34 +0300

    movie site web

--- udacity_intro/moviesite/entertainment.py
+++ udacity_intro/moviesite/entertainment.py
@@ -1,7 +1,9 @@
 import media
+import fresh_tomatoes
 
 terminator = media.Movie("Terminator", "Arnold going places", "http://cdn.emgn.com/wp-content/uploads/2015/07/JyTCM.jpg", "https://www.youtube.com/watch?v=c4Jo8QoOTQ4")
 
-print terminator.story
-
-terminator.show_trailer()
+#print terminator.story
+#terminator.show_trailer()
+movies = [terminator]
+fresh_tomatoes.open_movies_page(movies)
--- udacity_intro/moviesite/media.py
+++ udacity_intro/moviesite/media.py
@@ -1,11 +1,11 @@
 import webbrowser
 
 class Movie():
-	def __init__ (self, title, story, poster, trailer):
+	def __init__ (self, title, storyline, poster_image_url, trailer_youtube_url):
 		self.title = title
-		self.story = story
-		self.poster = poster
-		self.trailer = trailer
+		self.storyline = storyline
+		self.poster_image_url = poster_image_url
+		self.trailer_youtube_url = trailer_youtube_url
 
 	def show_trailer(self):
-		webbrowser.open(self.trailer)
+		webbrowser.open(self.trailer_youtube_url)
