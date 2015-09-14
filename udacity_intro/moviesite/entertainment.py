7990bdeed47aa73a2b825058700b407256f7dc33
avjgit <avjmail@gmail.com>
2015-09-14 12:49:43 +0300

    movie website

---
+++ udacity_intro/moviesite/entertainment.py
@@ -0,0 +1,7 @@
+import media
+
+terminator = media.Movie("Terminator", "Arnold going places", "http://cdn.emgn.com/wp-content/uploads/2015/07/JyTCM.jpg", "https://www.youtube.com/watch?v=c4Jo8QoOTQ4")
+
+print terminator.story
+
+terminator.show_trailer()
---
+++ udacity_intro/moviesite/media.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+class Movie():
+	def __init__ (self, title, story, poster, trailer):
+		self.title = title
+		self.story = story
+		self.poster = poster
+		self.trailer = trailer
+
+	def show_trailer(self):
+		webbrowser.open(self.trailer)
