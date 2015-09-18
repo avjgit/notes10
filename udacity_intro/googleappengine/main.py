b182a2900c0909b04092a66b410e17540e9a7440
avjgit <avjmail@gmail.com>
2015-09-18 08:37:48 +0300

    gae form

--- udacity_intro/googleappengine/main.py
+++ udacity_intro/googleappengine/main.py
@@ -16,10 +16,17 @@
 #
 import webapp2
 
+form = """
+<form action="http://www.google.com/search">
+	<input name="q">
+	<input type="submit">
+</form>
+"""
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    	self.response.headers['Content-Type'] = 'text/plain'
-    	self.response.out.write("hi hi ")
+    	# self.response.headers['Content-Type'] = 'text/plain'
+    	self.response.out.write(form)
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
