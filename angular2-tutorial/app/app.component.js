6d507ef49270b3e9fe229b8d61e9e5ac60b705aa
AJ <avjmail@gmail.com>
2016-07-11 15:23:27 +0300

    config fixes

--- angular2-tutorial/app/app.component.js
+++ angular2-tutorial/app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>the official angular.io tutorial</h1>'
+            template: '<h1>the official angular.io tutorial, yes it is 222 33</h1>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- angular2-tutorial/app/app.component.ts
+++ angular2-tutorial/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>the official angular.io tutorial</h1>'
+    template: '<h1>the official angular.io tutorial, yes it is 222 33</h1>'
 })
 
 export class AppComponent {}
