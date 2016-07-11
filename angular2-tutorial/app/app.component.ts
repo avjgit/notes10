8696c2ffc7d1dbfa8023510f37bc4c7ce2cc33c7
AJ <avjmail@gmail.com>
2016-07-11 16:56:29 +0300

    Editing Name property

--- angular2-tutorial/app/app.component.ts
+++ angular2-tutorial/app/app.component.ts
@@ -14,7 +14,10 @@ export class Hero {
 
 <h2>{{hero.name}} details!</h2>
 <div><label>id: </label>{{hero.id}}</div>
-<div><label>name: </label>{{hero.name}}</div>
+<div>
+    <label>name: </label>
+    <input value="{{hero.name}}" placeholder="name">    
+</div>
 `
 })
 export class AppComponent {
