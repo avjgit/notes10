5db987780610a1b5e90a3d7fa3de9f1f93e0a105
AJ <avjmail@gmail.com>
2016-07-11 15:49:10 +0300

    old folders removal

--- angular2-tutorial-/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'my-app',
-    template: '<h1>the official angular.io tutorial, yes it is 222 33</h1>'
-})
-
-export class AppComponent {}
