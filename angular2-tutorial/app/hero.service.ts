3c227fc04c3704ceee966bdeff18bb2ade2138c5
AJ <avjmail@gmail.com>
2016-07-13 16:03:07 +0300

    HTTP - fake backend

--- angular2-tutorial/app/hero.service.ts
+++ angular2-tutorial/app/hero.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
-
-import { HEROES } from './mock-heroes';
+import { Http } from '@angular/http';
+import { Hero } from './hero';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
-  getHeroes() {
-    return Promise.resolve(HEROES);
+  private heroesUrl = 'app/heroes';  // URL to web api
+
+  constructor(private http: Http) { }
+
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data)
+               .catch(this.handleError);
   }
 
   getHero(id: number) {
     return this.getHeroes()
                 .then(heroes => heroes.find(hero => hero.id === id));
   }
+
+  private handleError(error: any) {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+
 }
