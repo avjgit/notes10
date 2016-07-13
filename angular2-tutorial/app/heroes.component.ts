06f60a8b71af95d67375a12c8ed68b702347a9ab
AJ <avjmail@gmail.com>
2016-07-13 16:21:50 +0300

    Edit and delete

--- angular2-tutorial/app/heroes.component.ts
+++ angular2-tutorial/app/heroes.component.ts
@@ -10,8 +10,8 @@ import { Router } from '@angular/router';
     styleUrls: ['app/heroes.component.css'],
     selector: 'my-heroes',
     templateUrl: 'app/heroes.component.html',
-  directives: [HeroDetailComponent],
-  providers: []
+    directives: [HeroDetailComponent],
+    providers: []
 })
 export class HeroesComponent implements OnInit{
 
@@ -46,4 +46,27 @@ export class HeroesComponent implements OnInit{
     gotoDetail() {
       this.router.navigate(['/detail', this.selectedHero.id]);
     }
+
+    addHero() {
+      this.addingHero = true;
+      this.selectedHero = null;
+    }
+
+    close(savedHero: Hero) {
+      this.addingHero = false;
+      if (savedHero) { this.getHeroes(); }
+    }
+
+    deleteHero(hero: Hero, event: any) {
+      event.stopPropagation();
+      this.heroService
+          .delete(hero)
+          .then(res => {
+            this.heroes = this.heroes.filter(h => h !== hero);
+            if (this.selectedHero === hero) { this.selectedHero = null; }
+          })
+          .catch(error => this.error = error);
+    }
+
+
 }
