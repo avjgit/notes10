7026e226e3bb35058b82facf746665943fd20b0c
AJ <avjmail@gmail.com>
2016-07-13 15:10:30 +0300

    Hero Detail

--- angular2-tutorial/app/dashboard.component.ts
+++ angular2-tutorial/app/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'my-dashboard',
@@ -10,7 +11,9 @@ export class DashboardComponent implements OnInit {
 
     heroes: Hero[] = [];
 
-    constructor(private heroService: HeroService) {
+    constructor(
+        private heroService: HeroService,
+        private router: Router) {
 
     }
 
@@ -19,6 +22,9 @@ export class DashboardComponent implements OnInit {
             .then(heroes => this.heroes = heroes.slice(1, 5));
     }
 
-    gotoDetail() {
+    gotoDetail(hero: Hero) {
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
     }
+
 }
